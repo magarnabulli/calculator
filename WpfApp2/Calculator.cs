2a490868382a434073462db2f394167550535300
magarnabulli <112855853+magarnabulli@users.noreply.github.com>
2022-10-19 19:52:51 +0200

    lesgo

---
+++ WpfApp2/Calculator.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    internal class Calculator
+    {
+        public string Beräkna(string input)
+        {
+            try
+            {
+                string svar = Split(input);
+                return svar.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Beräkna ett tal i taget!");
+                return "";
+            }
+        }
+        public string ParantesFörst(string parantes)
+        {
+            string värdeIParantes = "";
+            for (int i = 0; i < parantes.Length; i++)
+            {
+                if (parantes[i].ToString() == "*")
+                {
+                    var multisplit = parantes.Split('*');
+                    return värdeIParantes = Multiplikation(multisplit);
+                }
+                if (parantes[i].ToString() == "-")
+                {
+                    var subsplit = parantes.Split('-');
+                    return värdeIParantes = Subtraktion(subsplit);
+                }
+                if (parantes[i].ToString() == "+")
+                {
+                    var addsplit = parantes.Split('+');
+                    return värdeIParantes = Addition(addsplit);
+                }
+                if (parantes[i].ToString() == "/")
+                {
+                    var divsplit = parantes.Split('/');
+                    return värdeIParantes = Division(divsplit);
+                }
+                if (parantes[i].ToString() == "%")
+                {
+                    var prosplit = parantes.Split('%');
+                    return värdeIParantes = Procent(prosplit);
+                }
+                if (parantes[i].ToString() == "√")
+                {
+                   
+                    var rotsplit = parantes.Split('√');
+                    return värdeIParantes = Rotenur(rotsplit[1]);
+                }
+            }
+
+            return värdeIParantes;
+        }
+        public string Split(string input)
+        {
+            string[] beräkna = new string[input.Length];
+            if (input.Contains("("))
+            {
+                var split1 = input.Split('(', ')');
+                string a = split1[1]; //detta tar bort paranteser och innehåller endast uträkningen i parantesen
+                string b = split1[2]; //innehåller andra delen
+                string färdig = ParantesFörst(a);
+                färdig += b;
+
+                input = färdig;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].ToString() == "*")
+                {
+                    beräkna = input.Split('*');
+                    return Multiplikation(beräkna);
+                }
+                if (input[i].ToString() == "+")
+                {
+                    beräkna = input.Split('+');
+                    return Addition(beräkna);
+                }
+                if (input[i].ToString() == "-")
+                {
+                    beräkna = input.Split('-');
+                    return Subtraktion(beräkna);
+                }
+                if (input[i].ToString() == "/")
+                {
+                    beräkna = input.Split('/');
+                    return Division(beräkna);
+                }
+                if (input[i].ToString() == "%")
+                {
+                    beräkna = input.Split('%');
+                    return Procent(beräkna);
+                }
+                if (input[i].ToString() == "√")
+                {
+                    beräkna = input.Split('√');
+                    return Rotenur(beräkna[1]);
+                }
+            }
+            return "";
+        }
+        public string Multiplikation(string[] numbers)
+        {
+            double nr1 = Convert.ToDouble(numbers[0]);
+            double nr2 = Convert.ToDouble(numbers[1]);
+            var sum = nr1 * nr2;
+
+            return sum.ToString();
+        }
+        public string Addition(string[] numbers)
+        {
+            double nr1 = Convert.ToDouble(numbers[0]);
+            double nr2 = Convert.ToDouble(numbers[1]);
+            double beräkna = nr1 + nr2;
+            return beräkna.ToString();
+        }
+        public string Subtraktion(string[] numbers)
+        {
+            double nr1 = Convert.ToDouble(numbers[0]);
+            double nr2 = Convert.ToDouble(numbers[1]);
+            double beräkna = nr1 - nr2;
+            return beräkna.ToString();
+        }
+        public string Division(string[] numbers)
+        {
+            double nr1 = Convert.ToDouble(numbers[0]);
+            double nr2 = Convert.ToDouble(numbers[1]);
+            double beräkna = nr1 / nr2;
+            return beräkna.ToString();
+        }
+        public string Procent(string[] numbers)
+        {
+            double nr1 = Convert.ToDouble(numbers[0]);
+            double nr2 = Convert.ToDouble(numbers[1]);
+            double beräkna = nr1 * (0.1f) * nr2 * (0.1f);
+            return beräkna.ToString();
+        }
+        public string Rotenur(string number)
+        {
+            double rotenur = Convert.ToDouble(number);
+            string rot = Math.Sqrt(rotenur).ToString();
+            return rot.ToString();
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq.Expressions;
+using System.IO;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+
+        Calculator myCalc = new Calculator();
+        string parantesÖppning = "(";
+        string parantesStängning = ")";
+        decimal button1 = 1;
+        decimal button2 = 2;
+        decimal button3 = 3;
+        decimal button4 = 4;
+        decimal button5 = 5;
+        decimal button6 = 6;
+        decimal button7 = 7;
+        decimal button8 = 8;
+        decimal button9 = 9;
+        decimal button0 = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e) // 1
+        {
+            screen.Text += button1;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e) //2
+        {
+            screen.Text += button2;
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)//3
+        {
+            screen.Text += button3;
+        }
+        private void Button_Click_5(object sender, RoutedEventArgs e)//4
+        {
+            screen.Text += button4;
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)//5
+        {
+            screen.Text += button5;
+        }
+        private void Button_Click_4(object sender, RoutedEventArgs e)//6
+        {
+            screen.Text += button6;
+        }
+        private void Button_Click_6(object sender, RoutedEventArgs e)//7
+        {
+            screen.Text += button7;
+        }
+        private void Button_Click_7(object sender, RoutedEventArgs e)//8
+        {
+            screen.Text += button8;
+        }
+        private void Button_Click_8(object sender, RoutedEventArgs e)//9
+        {
+            screen.Text += button9;
+        }
+        private void Button_Click_9(object sender, RoutedEventArgs e)//0
+        {
+            screen.Text += button0;
+        }
+        private void rensa_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = "";
+        }
+        private void subtraktion_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text += "-";
+        }
+        private void addition_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text += "+";
+        }
+        private void division_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text += "/";
+        }
+
+        private void procent_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text += "%";
+        }
+
+        private void comma_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text += ",";
+        }
+
+        private void multiplikation_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text += "*";
+
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void beräkna_Click(object sender, RoutedEventArgs e)
+        {
+            string klar = myCalc.Beräkna(screen.Text);
+            screen.Text = klar;
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            screen.Text += parantesÖppning;
+        }
+
+        private void parantes__Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text += parantesStängning;
+        }
+
+        private void rotenur_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text += "√";
+        }
+    }
+}
